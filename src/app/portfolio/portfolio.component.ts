56552446b68d2e6d067c6975aec7f163f831b910
SirKappa3000 <sjors2202@hotmail.com>
2024-06-03 16:57:52 +0200

    Change: first draft of homepage. added text and icons

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
