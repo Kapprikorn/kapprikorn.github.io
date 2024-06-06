94be86ed4be325d27b83334bf715619dae431c00
Kapprikorn <sjors2202@hotmail.com>
2024-06-06 17:32:11 +0200

    Change: added landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import {PortfolioComponent} from "./pages/portfolio/portfolio.component";
 import {IdleGameComponent} from "./pages/idle-game/idle-game.component";
 import {ZilchComponent} from "./pages/zilch/zilch.component";
+import {LandingPageComponent} from "./pages/landing-page/landing-page.component";
 
 export const routes: Routes = [
   {
-    path: '', component: PortfolioComponent
+    path: '', component: LandingPageComponent
+  },
+  {
+    path: 'portfolio', component: PortfolioComponent
   },
   {
     path: 'idle-game', component: IdleGameComponent
