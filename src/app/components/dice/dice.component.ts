6b51bbd2523369642f4556aab83b265f2829846b
Kapprikorn <sjors2202@hotmail.com>
2024-06-06 16:44:28 +0200

    Change: Added zilch structure and dice component styling

---
+++ src/app/components/dice/dice.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-dice',
+  standalone: true,
+  imports: [],
+  templateUrl: './dice.component.html',
+  styleUrl: './dice.component.css'
+})
+export class DiceComponent {
+  @Input() value: number | undefined;
+
+}
--- src/app/pages/zilch/zilch.component.ts
+++ src/app/pages/zilch/zilch.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import {DiceComponent} from "../../components/dice/dice.component";
+
+interface Dice {
+  id: number;
+  value: number;
+}
 
 @Component({
   selector: 'app-zilch',
   standalone: true,
-  imports: [],
+  imports: [
+    DiceComponent
+  ],
   templateUrl: './zilch.component.html',
   styleUrl: './zilch.component.css'
 })
 export class ZilchComponent {
+  dice: Dice[] = [
+    {
+      id: 1,
+      value: 1,
+    },
+    {
+      id: 2,
+      value: 2,
+    },
+    {
+      id: 3,
+      value: 3,
+    },
+    {
+      id: 4,
+      value: 5,
+    },
+    {
+      id: 5,
+      value: 5,
+    },
+    {
+      id: 6,
+      value: 6,
+    }
+  ];
 
 }
