e841004c762d55ff45ad653b29513b4879d71ded
Kapprikorn <sjors2202@hotmail.com>
2024-06-09 15:01:27 +0200

    Change: moved content from landing page to portfolio. Moved styling to generic section.

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {MatCard} from "@angular/material/card";
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    MatCard
   ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
